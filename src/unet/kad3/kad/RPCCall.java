052dc506f7f202343ec111c493c1e42482a2ddad
DrBrad <brad@bradeagle.com>
2024-02-26 14:48:18 -0700

    Expected RPC Call

--- src/unet/kad3/kad/RPCCall.java
+++ src/unet/kad3/kad/RPCCall.java
@@ -1,14 +1,34 @@
 package unet.kad3.kad;
 
 import unet.kad3.messages.inter.MessageBase;
+import unet.kad3.utils.UID;
 
 public class RPCCall {
 
     private MessageBase req, res;
+    private UID expectedUID;
+    private boolean sourceWasKnownReachable, socketMismatch;
+    private long expectedRTT;
     private RPCState state = RPCState.UNSENT;
 
     public RPCCall(MessageBase message){
         this.req = message;
+    }
+
+    public void setExpectedUID(UID uid){
+        this.expectedUID = uid;
+    }
+
+    public boolean knownReachableAtCreationTime(){
+        return sourceWasKnownReachable;
+    }
+
+    public void setExpectedRTT(long rtt){
+        expectedRTT = rtt;
+    }
+
+    public long getExpectedRTT(){
+        return expectedRTT;
     }
 
     /*
