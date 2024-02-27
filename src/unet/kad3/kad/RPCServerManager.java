09413a36280c2699aba5cc461d440c786d77427b
DrBrad <brad@bradeagle.com>
2024-02-27 00:13:14 -0700

    Derived UID

---
+++ src/unet/kad3/kad/RPCServerManager.java
@@ -0,0 +1,10 @@
+package unet.kad3.kad;
+
+import unet.kad3.kad.dht.inter.DHT;
+
+public class RPCServerManager {
+
+    public RPCServerManager(DHT dht){
+
+    }
+}
---
+++ src/unet/kad3/kad/dht/inter/DHT.java
@@ -0,0 +1,6 @@
+package unet.kad3.kad.dht.inter;
+
+public class DHT {
+
+
+}
